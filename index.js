0388317353e11230d0696508c4ce8e095331d6dd
memo <mehmetbaki.123@gmail.com>
2022-04-10 23:48:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    messageList=[]
+for (let name of names) {
+    messageList.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+
+    
+}
+console.log(messageList)
+return messageList
+
+}
+
+writeCards(["omer","ali","mehmet"],"dogumguu")
+
+function countDown(param) {
+    while (param>0) {
+        if (param>0) {
+            console.log(param)
+            param--
+            if(param=="0"){console.log(param)}
+        }
+}}
+
+countDown(10)
